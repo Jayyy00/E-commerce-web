03c547b0cdd051ccd13feefd57a8a2b77106d24a
J.A Jayani Jayasinghe <99647114+Jayyy00@users.noreply.github.com>
2024-07-28 17:35:59 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+i
+
+const Navbar = ()=>{
+    return (
+        <div>
+            {/*upper navbar*/}
+            
+            {/* ;ower navbar*/}
+            <div></div>
+        </div>
+    )
+}
