7c108793042b6bf777916401463382d52f893d75
J.A Jayani Jayasinghe <99647114+Jayyy00@users.noreply.github.com>
2024-08-05 06:44:49 +0530

    lower nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
-  return <div>
+  return (<div>
     <Navbar/>
-  </div>;
+    <Hero/>
+  </div>
+  );
 };
 
 export default App;
--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -4,25 +4,41 @@ import DarkButton from "../assets/website/Dark-mode.png"; // Assuming you have a
 
 const DarkMode = () => {
   const [theme, setTheme] = React.useState(
-    localStorage.getItem("theme")? localStorage.getItem("theme"):"light"// "light"
+    localStorage.getItem("theme") || "light"
   );
-  const element = document.documentElement;
-  console.log(element);//end
+  const element = document.documentElement; // html element
+
+  React.useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
 
   return (
     <div className="relative">
       <img
         src={LightButton}
         alt="Light Mode Button"
-        className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${
+          theme === "dark" ? "opacity-0" : "opacity-100"
+        }`}
       />
       <img
         src={DarkButton}
         alt="Dark Mode Button"
-        className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 ${
+          theme === "light" ? "opacity-0" : "opacity-100"
+        }`}
       />
     </div>
   );
 };
 
 export default DarkMode;
+
